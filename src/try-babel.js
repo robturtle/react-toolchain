c64370aa976ed5238d49dee1c4a8384ad0fd46e8
Yang Liu <jeremyrobturtle@gmail.com>
2017-04-26 21:51:54 -0400

    try webpack

--- src/try-babel.js
+++ src/try-babel.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const x = 3;
 
 let hello = a => console.log(`hello, ${a}`);
@@ -5,3 +7,5 @@ let hello = a => console.log(`hello, ${a}`);
 hello(x);
 
 let jsx = <p class="st" style="color: green;">Hello, JSX!</p>;
+
+export default jsx;
