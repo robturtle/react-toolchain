25b6a2425974ad8936033fb1597344d1bd2f370f
Yang Liu <jeremyrobturtle@gmail.com>
2017-04-27 11:29:17 -0400

    library splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-module.exports = {
-  context: __dirname + "/lib",
-  entry: "./try-webpack.js",
-  output: {
-    path: __dirname + "/dist",
-    filename: "bundle.js",
-    chunkFilename: "[id].bundle.js"
-  }
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = function(env) {
+  return {
+    context: path.resolve(__dirname, 'lib'),
+    entry: {
+      main: './try-webpack.js'
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[chunkhash].js'
+    },
+    plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: function (module) {
+          return module.context && module.context.indexOf('node_modules') !== -1;
+        }
+      }),
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'manifest'
+      })
+    ]
+  };
 };
