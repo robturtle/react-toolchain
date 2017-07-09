64bded8712541e7991cb14a41c8044ed4a50100f
Yang Liu <jeremyrobturtle@gmail.com>
2017-07-09 01:36:07 -0400

    fix: db-test error handling

--- src/server/db-test.js
+++ src/server/db-test.js
@@ -4,6 +4,7 @@ const Client = require('pg').Client;
 const client = new Client(DATABASE_URL);
 client.connect();
 client.query('select * from test_table', (err, result) => {
-  console.log(JSON.stringify(result.rows));
+  if (err) console.error(err);
+  else console.log(JSON.stringify(result.rows));
   client.end();
 });
