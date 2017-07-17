af1fa64bab4bca0261ad74db4c782df433061151
Yang Liu <jeremyrobturtle@gmail.com>
2017-07-17 17:26:45 -0400

    user existence check

--- src/data/graphql/resolvers/user.js
+++ src/data/graphql/resolvers/user.js
@@ -2,6 +2,23 @@ import {
   User,
 } from '../../models';
 
+async function findUser(info, expectExists = true) {
+  const user = await User.findOne({ where: { name: info.name } });
+  if (!expectExists) {
+    if (user) {
+      throw Error('User name already exists!');
+    }
+    if (await User.findOne({ where: { email: info.email } })) {
+      throw Error('User email alreay exists!');
+    }
+  }
+  if (!user && expectExists) {
+    throw Error('User does not exist!')
+  }
+
+  return user;
+}
+
 export default {
   users: () => User.findAll(),
 
@@ -13,22 +30,24 @@ export default {
     return User.findOne({ where: { name: ref } });
   },
 
-  createUser: ({ info }) => User.create(info).then(u => u),
+  createUser: async ({ info }) => {
+    await findUser(info, false);
+    const user = await User.create(info);
+
+    return user;
+  },
 
   updateUser: async ({ info }) => {
-    const user = await User.findOne({ where: { name: info.name } });
+    const user = await findUser(info);
     await user.update(info);
 
     return user;
   },
 
   deleteUser: async ({ info }) => {
-    const userExists = await User.findOne({ where: {
-      email: info.email,
-      name: info.name,
-     } }) !== null;
+    await findUser(info);
     await User.destroy({ where: { name: info.name } });
 
-    return userExists;
+    return true;
   },
 };
